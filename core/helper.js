7f8a040c0519b3343cce75de47f0562138c0c9e1
lboleslavsky <lukas.boleslavsky@gmail.com>
2014-05-19 20:28:55 +0200

    Initial commit. Moving from sourceforge :)

---
+++ core/helper.js
@@ -0,0 +1,111 @@
+/**
+* Parse markups
+* options
+*/
+function parseCustomMark(options) {
+options.Element.text("");
+$('#board').html('');
+var p = options.Text;
+var tags = Array();    
+tags['video'] = function (params, options) {
+if (params['src'] == undefined) {
+return;
+}
+$("<div></div>").html(options.Text).appendTo(options.Element.selector);
+$('<iframe width="560" height="315" src="' + params['src'] + '" frameborder="0" allowfullscreen></iframe>').html('').appendTo('#board');
+return;
+};
+
+tags['link'] = function (params, options) {
+if (params['src'] == undefined) {
+return;
+}
+if (params['text'] != undefined) {
+params['text'] = params['text'].replace('_', '&nbsp;');
+}
+
+$("<div></div>").html(options.Text + "&nbsp;").appendTo(options.Element.selector);
+$('<a href="' + params['src'] + '" target="_blank"></a>').html(params['text']).appendTo(options.Element.selector);
+return;
+};
+
+tags['nl'] = function (params, options) {
+$("<div></div>").html(options.Text).appendTo(options.Element.selector);
+$('<br/>').html('').appendTo(options.Element.selector);
+return;
+};
+
+var n = 0;
+var m = 0;
+var b = 0;
+var txt = "";
+var isFound = false;
+while (true) {
+b = n;
+n = p.indexOf('{[', n);
+if (n == -1) {
+if (isFound) {
+//txt += p.substring(b + 2, p.length);
+$("<div></div>").html(p.substring(b + 2, p.length)).appendTo(options.Element.selector);
+return;
+}
+options.Element.text(options.Text);
+return p;
+}
+m = p.indexOf(']}', n + 2);
+if (m == -1) {
+if (isFound) {
+//txt += p.substring(b + 2, p.length);
+$("<div></div>").html(p.substring(b + 2, p.length)).appendTo(options.Element.selector);
+return;
+}
+options.Element.text(options.Text);
+return p;
+}
+isFound = true;
+if (txt != "") {
+b += 2;
+}
+txt += p.substring(b + 2, n);
+var f = p.substring(n + 2, m);
+
+var a = f.split(' ');
+if (a[0] == undefined || tags[a[0]] == undefined) {
+n = m;
+continue;
+}
+var element = tags[a[0]](getAttrParams(a), { Element: options.Element, Text: p.substring(b, n) });
+if (element != undefined) {
+txt += element;
+}
+n = m;
+}
+function getAttrParams(a) {
+params = Array();
+for (i = 1; i < a.length; i++) {
+var attr = a[i].split('=');
+if (attr[0] == undefined || attr[1] == undefined) {
+continue;
+}
+params[attr[0]] = attr[1];
+}
+return params;
+}
+}
+
+/**
+* How to behave. 
+* avatar - current avatar
+*/
+function basicBehavior(avatar)
+{
+    $('#' + avatar.getName()).mouseover(function () {
+        avatar.about();
+    });
+    $('#' + avatar.getName()).mouseout(function () {
+        /*if (temp.length == 0) {
+            return;
+        }*/
+        avatar.repeat();
+    });
+}
